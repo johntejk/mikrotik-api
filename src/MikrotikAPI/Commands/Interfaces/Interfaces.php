9d54614e2c2ab1f5843e084aef1cd967ac96b39a
A S M Saief <anovob@users.noreply.github.com>
2018-08-01 13:26:07 +0600

    Update Interfaces.php

--- src/MikrotikAPI/Commands/Interfaces/Interfaces.php
+++ src/MikrotikAPI/Commands/Interfaces/Interfaces.php
@@ -165,5 +165,123 @@ class Interfaces {
     public function PPTPServer() {
         return new PPTPServer($this->roar);
     }
+    
+    public function getAll() {
+        $sentence = new SentenceUtil();
+        $sentence->fromCommand("/interface/getall");
+        $this->roar->send($sentence);
+        $rs = $this->roar->getResult();
+        return $rs->getResultArray();
+    }
+
+    /**
+     * This method is used to display one interface  
+     * in detail based on the id
+     * @param type $param array
+     * @param type $id string
+     * @return type array
+     */
+    public function set($param, $id) {
+        $sentence = new SentenceUtil();
+        $sentence->addCommand("/interface/set");
+        foreach ($param as $name => $value) {
+            $sentence->setAttribute($name, $value);
+        }
+        $sentence->where(".id", "=", $id);
+        $this->roar->send($sentence);
+        return "Success";
+    }
+
+    /**
+     * This method is used to enable interface by id
+     * @param type $id string
+     * @return type array
+     */
+    public function enable($id) {
+        $sentence = new SentenceUtil();
+        $sentence->addCommand("/interface/enable");
+        $sentence->where(".id", "=", $id);
+        $enable = $this->roar->send($sentence);
+        return "Success";
+    }
+
+    /**
+     * This method is used to disable interface by id
+     * @param type $id string
+     * @return type array
+     */
+    public function disable($id) {
+        $sentence = new SentenceUtil();
+        $sentence->addCommand("/interface/disable");
+        $sentence->where(".id", "=", $id);
+        $enable = $this->roar->send($sentence);
+        return "Success";
+    }
+
+    /**
+     * This method is used to display one interafce 
+     * in detail based on the id
+     * @param type $id string
+     * @return type array
+     * 
+     */
+    public function detailById($id) {
+        $sentence = new SentenceUtil();
+        $sentence->fromCommand("/interface/print");
+        $sentence->where(".id", "=", $id);
+        $this->roar->send($sentence);
+        $rs = $this->roar->getResult();
+        $i = 0;
+        if ($i < $rs->size()) {
+            return $rs->getResultArray();
+        } else {
+            return "No Interface With This id = " . $id;
+        }
+    }
+
+    /**
+     * This method is used to display one interafce
+     * in detail based on the name
+     * @param type $name string
+     * @return type array
+     *
+     */
+    public function detailByName($id) {
+        $sentence = new SentenceUtil();
+        $sentence->fromCommand("/interface/print");
+        $sentence->where("name", "=", $name);
+        $this->roar->send($sentence);
+        $rs = $this->roar->getResult();
+        $i = 0;
+        if ($i < $rs->size()) {
+            return $rs->getResultArray();
+        } else {
+            return "No Interface With This Name = " . $name;
+        }
+    }
+
+    /**
+     * This method is used to display one interafce
+     * get Id By name
+     * @param type $name string not array
+     * @return type array
+     *
+     */
+    public function getId($name) {
+        $sentence = new SentenceUtil();
+        $sentence->fromCommand("/interface/print");
+        $sentence->where("name", "=", $name);
+        $this->roar->send($sentence);
+        $rs = $this->roar->getResult();
+        $i = 0;
+        if ($i < $rs->size()) {
+            $rsArray = $rs->getResultArray();
+            foreach ($rsArray as $Id) {
+                return $Id['.id'];
+            }
+        } else {
+            return "No Interface With This Name = " . $name;
+        }
+    }
 
 }
